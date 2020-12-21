3cd9c016663c3ede91b3008d41598abe7eeacb87
Mark Fraser <markfraser2015@yahoo.com>
2020-12-20 19:35:05 -0800

    Move PLA file for consistent naming

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,9 +1,9 @@
 set(sources
-    ${CMAKE_SOURCE_DIR}/src/optimalpla.cpp
+    ${CMAKE_SOURCE_DIR}/src/pla.cpp
     ${CMAKE_SOURCE_DIR}/src/perceptron.cpp
 )
 
 set(headers
-    ${CMAKE_SOURCE_DIR}/include/optimalpla.hpp
+    ${CMAKE_SOURCE_DIR}/include/pla.hpp
     ${CMAKE_SOURCE_DIR}/include/perceptron.hpp
 )
